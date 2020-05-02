54ce5c0770f6d04c7960b443c610f76a03f50707
Jinay Shah <jinayshah86@gmail.com>
2020-05-02 17:32:04 +0530

    Fix typo

--- CtCI-6th-Edition/Chapter1/1_1/is_unique_1.py
+++ CtCI-6th-Edition/Chapter1/1_1/is_unique_1.py
@@ -1,7 +1,7 @@
 # Q. Implement an algorith to determine if a string has all the unique
 # characters. What if you cannot use additional data structures?
 # Time complexity: O(N); N is the length of the string
-# Space complexity: O(N): N is the length of the string
+# Space complexity: O(N); N is the length of the string
 
 import unittest
 

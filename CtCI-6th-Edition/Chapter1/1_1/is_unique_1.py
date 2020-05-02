b66755f6a569ba64170de9fbe5307918d83a425a
Jinay Shah <jinayshah86@gmail.com>
2020-05-02 16:00:33 +0530

    CTCI|CH1|1.1|T:O(N)|S:O(N)

---
+++ CtCI-6th-Edition/Chapter1/1_1/is_unique_1.py
@@ -0,0 +1,27 @@
+# Q. Implement an algorith to determine if a string has all the unique
+# characters. What if you cannot use additional data structures?
+# Time complexity: O(N); N is the length of the string
+# Space complexity: O(N): N is the length of the string
+
+import unittest
+
+
+def is_unique(string):
+    character_map = {}
+    for character in string:
+        if character in character_map:
+            return False
+        character_map[character] = True
+    return True
+
+
+class TestIsUniqueString(unittest.TestCase):
+    def test_pass(self):
+        self.assertTrue(is_unique("mickey"))
+
+    def test_fail(self):
+        self.assertFalse(is_unique("minnie"))
+
+
+if __name__ == "__main__":
+    unittest.main()
